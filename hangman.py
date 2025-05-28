bf767ed22ccd84f6aa99da527da1e06a4159dccf
Mostafa <mostafa.torbati@gmail.com>
2025-05-28 15:23:18 +0200

    make the code cleaner and done TODO-1 to TODO-3

--- hangman.py
+++ hangman.py
@@ -6,11 +6,12 @@ import random
 chosen_word = random.choice(word_list)
 print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = []
-gussing = input("Guess a letter: \n")
-guess.append(gussing.lower())
+guess = input("Guess a letter: \n").lower()
 print(guess)
 #TODO-3 - Check if the letter user guessed (guess) is one of the letters in the chosen_word. print "Right" if it is, "Wrong" if it's not.
 
-
+if guess in chosen_word:
+    print("Right")
+else:
+    print("Wrong")
             
